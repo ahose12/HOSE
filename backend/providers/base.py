ebbd40aa2e2dfc717c969a2368c3dcbaa6eeaec4
ahose12 <103284843+ahose12@users.noreply.github.com>
2026-08-14 10:44:01 -0500

    Delete backend directory

--- backend/providers/base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC,abstractmethod
-class ListingProvider(ABC):
- @abstractmethod
- def discover(self):...
- @abstractmethod
- def images(self,listing):...
- def automation_allowed(self):return False
- def image_storage_allowed(self):return False
